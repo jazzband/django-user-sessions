8e43d6831404b8e55007379260c5f55c9bc518ef
Bouke Haarsma <bouke@webatoom.nl>
2014-01-11 09:45:19 +0100

    Released 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-user-sessions',
-    version='0.1.3-dev',
+    version='0.1.3',
     description='Django sessions with a foreign key to the user',
     long_description=open('README.rst').read(),
     author='Bouke Haarsma',
