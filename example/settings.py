bb951ca57ade7bcbec29412b33c688f1b014841a
Bouke Haarsma <bouke@haarsma.eu>
2015-05-14 09:38:38 +0200

    Updated example site settings

--- example/settings.py
+++ example/settings.py
@@ -1,31 +1,21 @@
-"""
-Django settings for example project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(__file__)
-
-SECRET_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'user_sessions',
-)
+PROJECT_PATH = os.path.dirname(__file__)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_PATH, 'database.sqlite3'),
+        'ATOMIC_REQUESTS': True,
+    }
+}
+
+STATIC_URL = '/static/'
+
+SECRET_KEY = 'DO NOT USE THIS KEY!'
 
 MIDDLEWARE_CLASSES = (
     'example.middleware.SetRemoteAddrFromForwardedFor',
@@ -40,45 +30,25 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'example.urls'
 
-WSGI_APPLICATION = 'example.wsgi.application'
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'templates'),
+)
 
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'user_sessions',
+)
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ATOMIC_REQUESTS': True,
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Custom configuration
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
-
 SESSION_ENGINE = 'user_sessions.backends.db'
-GEOIP_PATH = os.path.join(BASE_DIR, 'GeoLiteCity.dat')
+
+GEOIP_PATH = os.path.join(PROJECT_PATH, 'GeoLiteCity.dat')
+
 LOGIN_URL = '/admin/'
