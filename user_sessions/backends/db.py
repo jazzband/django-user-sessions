ae5080bbaa6508af40d06399b21b322b9525f7f9
Remi <r.fatcheung@gmail.com>
2016-12-15 14:24:22 -0500

    Don't empty _session_cache during create on SessionStore
    Starting Django 1.7 the create() method of db/SessionStore removes this line.
    
    I noticed an issue when using the SessionStore manually with Django 1.9 and django-user-sessions:
    ```
    ipdb>session = import_module(settings.SESSION_ENGINE).SessionStore(some_session_key)
    ipdb>session['foo'] = 'bar'
    ipdb>print session.get('foo')
    bar
    ipdb> session.create()
    ipdb> print session.get('foo')
    None
    ```
    I'm not 100% sure how it'll impact Django 1.6 and older but considering the support was dropped in 2014, maybe it won't impact so many people.

--- user_sessions/backends/db.py
+++ user_sessions/backends/db.py
@@ -58,7 +58,6 @@ class SessionStore(SessionBase):
                 # Key wasn't unique. Try again.
                 continue
             self.modified = True
-            self._session_cache = {}
             return
 
     def save(self, must_create=False):
