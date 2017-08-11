b483c7abefa889aa59bc5d0a3e2c4eb2d9e22c40
Bouke Haarsma <bouke@haarsma.eu>
2017-08-11 19:56:37 +0200

    Don’t capture the outer exception

--- user_sessions/templatetags/user_sessions.py
+++ user_sessions/templatetags/user_sessions.py
@@ -98,7 +98,7 @@ def location(value):
     """
     try:
         location = geoip() and geoip().city(value)
-    except Exception as e:
+    except:
         try:
             location = geoip() and geoip().country(value)
         except Exception as e:
