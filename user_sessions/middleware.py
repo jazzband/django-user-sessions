67b6709f58bbd5dd601bb34dc0d1f0bb6bc772b9
Magnun Leno <magnunleno@users.noreply.github.com>
2018-03-19 15:21:48 -0300

    Adds supporte for X-FORWARED_FOR
    If there is a `X-FORWARDED-FOR` header variable set, use it instead of the `REMOTE_ADDR`.

--- user_sessions/middleware.py
+++ user_sessions/middleware.py
@@ -24,7 +24,7 @@ class SessionMiddleware(MiddlewareMixin):
         engine = import_module(settings.SESSION_ENGINE)
         session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
         request.session = engine.SessionStore(
-            ip=request.META.get('REMOTE_ADDR', ''),
+            ip=request.META.get('HTTP_X_FORWARDED_FOR', '') or request.META.get('REMOTE_ADDR', ''),
             user_agent=request.META.get('HTTP_USER_AGENT', ''),
             session_key=session_key
         )
