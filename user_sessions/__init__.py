12bfbf8847050c7e8634db1e96ef41bcf187f873
blag <blag@users.noreply.github.com>
2023-09-19 01:24:05 -0700

    Use importlib.metadata instead of pkg_resources

--- user_sessions/__init__.py
+++ user_sessions/__init__.py
@@ -1,7 +1,12 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
 try:
-    __version__ = get_distribution("django-user-sessions").version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = None
+    from importlib.metadata import version
+except ImportError:
+    from pkg_resources import DistributionNotFound, get_distribution
+
+    try:
+        __version__ = get_distribution("django-user-sessions").version
+    except DistributionNotFound:
+        # package is not installed
+        __version__ = None
+else:
+    __version__ = version("django-user-sessions")
