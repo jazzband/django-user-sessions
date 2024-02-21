6fc87ced61d7cc59213d459f32d04097e11e696a
Adam Taylor <ataylor32@gmail.com>
2024-02-20 19:00:39 -0700

    Added the "UserSessionsConfig" class

---
+++ user_sessions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UserSessionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user_sessions'
+    verbose_name = 'User Sessions'
